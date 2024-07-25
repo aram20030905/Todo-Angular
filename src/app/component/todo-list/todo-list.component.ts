8b416b4e5fa398fe493368e6048ae743892b3e32
Aram Badalyan <aram.badalyan1.y@tumo.org>
2024-07-25 13:28:14 +0400

    editing

--- src/app/component/todo-list/todo-list.component.ts
+++ src/app/component/todo-list/todo-list.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import {NgClass, NgForOf, NgIf} from "@angular/common";
 import {ToDo} from "../../models/todo";
 import {TodoService} from "../../Services/todo.service";
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-todo-list',
@@ -9,23 +10,30 @@ import {TodoService} from "../../Services/todo.service";
   imports: [
     NgForOf,
     NgClass,
-    NgIf
+    NgIf,
+    FormsModule
   ],
   templateUrl: './todo-list.component.html',
   styleUrl: './todo-list.component.css'
 })
 export class TodoListComponent {
 todos : ToDo[]= [];
+  isGoing=false;
   constructor(private todoService:TodoService) {
   }
   ngOnInit(){
 this.fetchTodos()
   }
-  onAdd(value:string){
-    const todo=new ToDo("-1", value,false)
+  onAdd(inputElement:HTMLInputElement){
+    if(!inputElement.value){
+
+      return
+    }
+    const todo=new ToDo("-1", inputElement.value,false)
     console.log(todo)
    const newTodo= this.todoService.create(todo)
-    this.todos.push(newTodo)
+    this.todos.unshift(newTodo)
+    inputElement.value=""
   }
 
   onDelete(id:string) {
@@ -44,8 +52,10 @@ this.fetchTodos()
 
   onEdit(toEdit:ToDo) {
 
-toEdit.isEditing=true;
-
+    if(!this.isGoing) {
+      toEdit.isEditing = true;
+    }
+    this.isGoing=true
   }
   private findById(id:string):ToDo | undefined{
     return this.todos.find(todo=>todo.id===id)
@@ -55,14 +65,24 @@ toEdit.isEditing=true;
     todo.isEditing=false
     const index= this.todos.findIndex(value => value.id === todo.id)
     this.todos[index]={...this.todos[index]}
+    this.isGoing=false
   }
 
+  onDoneChange(todo:ToDo){
+    this.updateTodo(todo)
+  }
   onSave(todo: ToDo, title:string) {
     todo.title=title;
     todo.isEditing=false
-   const updatedTodo= this.todoService.update(todo)
+    this.updateTodo(todo)
+    this.isGoing=false
+
+
+  }
+  private updateTodo(todo:ToDo){
+    const updatedTodo= this.todoService.update(todo)
     if (updatedTodo){
-     const index= this.todos.findIndex(value => value.id === updatedTodo.id)
+      const index= this.todos.findIndex(value => value.id === updatedTodo.id)
       this.todos[index]={...updatedTodo}
     }
 
